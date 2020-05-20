e35a9dfe3bf64b28eae084da8f8758b483b18b87
amir-shachar <54116224+amir-shachar@users.noreply.github.com>
2020-05-20 17:48:53 +0300

    include cases of separated digits.

--- string.generator/src/main/StringGeneratorImpl.java
+++ string.generator/src/main/StringGeneratorImpl.java
@@ -12,12 +12,58 @@ public class StringGeneratorImpl implements StringGenerator
         StringPattern stringPattern = new StringPattern(pattern);
         List<String> list = stringPattern.getRepresentation();
         Set<String> generatedStrings = new HashSet<>();
-        int curValue = 0;
-        while(maxNumericValueAchieved(list) && generatedStrings.size() < size)
+        int curValue = rangeFrom;
+        while(maxNumericValueAchieved(list, curValue) && generatedStrings.size() < size)
         {
-            generatedStrings.add(insertValueIntoString(list));
+            generatedStrings.add(insertValueIntoString(list, curValue));
+            curValue++;
         }
 
         return generatedStrings;
+    }
+
+    private boolean maxNumericValueAchieved(List<String> list, int value)
+    {
+        return Math.pow(10, getNumberOfDigits(list)) > value;
+    }
+
+    private int getNumberOfDigits(List<String> list)
+    {
+        return list.stream().filter(x -> x.contains("*")).map(String::length).reduce(Integer::sum).orElse(0);
+    }
+
+    private String insertValueIntoString(List<String> list, int curValue)
+    {
+        int index =0;
+        String numericValue = String.valueOf(curValue);
+        int stringIndex = 0;
+        StringBuilder builder = new StringBuilder();
+        while(index < list.size())
+        {
+            if(list.get(index).contains("*"))
+            {
+                int len = list.get(index).length();
+
+                while( len > 0)
+                {
+                    if(stringIndex < numericValue.length())
+                    {
+                        builder.append(numericValue.charAt(stringIndex));
+                        stringIndex++;
+                    }
+                    else
+                    {
+                        builder.append("0");
+                    }
+                    len--;
+                }
+            }
+            else
+            {
+                builder.append(list.get(index));
+            }
+            index++;
+        }
+        return builder.toString();
     }
 }
